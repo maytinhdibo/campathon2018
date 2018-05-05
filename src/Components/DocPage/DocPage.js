259f4ef4ca95816848025781b000acb52060698d
hpbdmylove <iammaytinhdibo@gmail.com>
2018-05-06 03:04:26 +0700

    dsd

--- src/Components/DocPage/DocPage.js
+++ src/Components/DocPage/DocPage.js
@@ -20,21 +20,6 @@ class DocPage extends React.Component {
                 });
             })
     }
-    componentDidUpdate(){
-        document.getElementsByClassName('nav')[0].style.left = '-1000px';
-        const self = this;
-        const  id = self.props.match.params.id;
-        fetch('http://localhost:8080/output?file='+id+'/doc.html&&read=true')
-            .then(function (response) {
-                response.text().then(function (text) {
-                    document.getElementById('doc-content').innerHTML = text;
-                    self.state={
-                        text:text
-                    }
-                    self.setState(self.state);
-                });
-            })
-    }
     render(){
         return(
             <div>
